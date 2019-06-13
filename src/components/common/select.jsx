281715da2b70ad69bb8e36b85aad90460903c8e9
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2019-06-13 14:49:16 +0300

    Fixes error of attribute

--- src/components/common/select.jsx
+++ src/components/common/select.jsx
@@ -3,9 +3,7 @@ import React from "react";
 const Select = ({ classes, label, options }) => {
   return (
     <select className={classes}>
-      <option value="" selected disabled>
-        {label}
-      </option>
+      <option value="">{label}</option>
       {options.map(option => (
         <option key={option} value={option}>
           {option}
