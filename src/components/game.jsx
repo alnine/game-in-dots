66fbb3a98c19b30fdfe2b1c7e6ff047179e2f5cd
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2019-06-19 00:20:25 +0300

    Implements game logic

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -10,7 +10,11 @@ class Game extends Component {
     board: [],
     playerName: "",
     settings: {},
-    leaders: []
+    leaders: [],
+    result: {
+      player: 0,
+      computer: 0
+    }
   };
 
   async componentDidMount() {
@@ -27,11 +31,72 @@ class Game extends Component {
     return Array(size ** 2).fill(null);
   };
 
+  handlePlayClick = () => {
+    this.playLevel();
+  };
+
+  playLevel = () => {
+    const { board } = this.state;
+    const filtered = this.getFilteredArrayIndex(board, null);
+    const square = this.getRandomSquareIndex(filtered);
+    this.fillBlue(square);
+    setTimeout(() => this.checkResult(square), 900);
+  };
+
+  checkResult = square => {
+    const { board } = this.state;
+
+    if (board[square] === "green") {
+      this.addPoint("player");
+    } else {
+      this.fillRed(square);
+      this.addPoint("computer");
+    }
+
+    this.hasWinner();
+  };
+
+  hasWinner = () => {
+    const { player, computer } = this.state.result;
+    const boardSize = this.state.board.length;
+
+    if (computer / boardSize <= 0.5 && player / boardSize <= 0.5) {
+      this.playLevel();
+    }
+  };
+
+  addPoint = scorer => {
+    const { result } = { ...this.state };
+    result[scorer] += 1;
+    this.setState({ result });
+  };
+
+  fillBlue = index => {
+    const board = [...this.state.board];
+    board[index] = "blue";
+    this.setState({ board });
+  };
+
+  fillRed = index => {
+    const board = [...this.state.board];
+    board[index] = "red";
+    this.setState({ board });
+  };
+
+  getFilteredArrayIndex = (arr, cond) => {
+    return arr
+      .map((item, index) => (item === cond ? index : null))
+      .filter(i => i !== null);
+  };
+
+  getRandomSquareIndex = arr => {
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
+
   handleClickOnSquare = i => {
-    console.log(this.state);
     const board = [...this.state.board];
     if (board[i] === "blue") {
-      board[i] = "red";
+      board[i] = "green";
       this.setState({ board });
     }
     return;
@@ -70,6 +135,7 @@ class Game extends Component {
               player={playerName}
               onSelectChange={this.handleChangeOnSelect}
               onInputChange={this.handleChangeOnInput}
+              onPlayClick={this.handlePlayClick}
             />
             <Board
               data={board}
